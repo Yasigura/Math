807e4e67ccba4ed6d5045fcdb8f77d6d15b3315b
Yasigura <lyguska228@gmail.com>
2025-03-17 16:01:13 +0300

    update code

--- math.py
+++ math.py
@@ -1,30 +1,26 @@
-a = float (input('Введите коэфициент a:'))
-b = float (input('Введите коэфициент b:'))
-c = float (input('Введите коэфициент c:'))
-discr = b**2 - 4*a*c
-print('Дискрименант =', discr)
-if discr > 0:
-    print('Есть два корня:')
-    x1 = (-b + discr**0.5) / (2*a)
-    x2 = (-b - discr**0.5) / (2*a)
-    print('Первый корень:', x1)
-    print('Второй корень:', x2)
-elif discr == 0:
-    print('Есть один корень:')
-    x = -b / (2*a)
-    print('Один корень:', x) 
-elif discr < 0:
-    print('Корней нет.')
-input ()
- 
 
 
+def test():
+    a = float(input("Введите коэффициент a: "))
+    if a == 0:
+        print("Ошибка: коэффициент a не может быть равен 0 (уравнение не будет квадратным).")
+        input("Нажмите Enter для завершения...")
+        return
+    b = float(input("Введите коэффициент b: "))
+    c = float(input("Введите коэффициент c: "))
+    discr = b ** 2 - 4 * a * c
+    print("Дискриминант =", discr)
+    if discr > 0:
+        x1 = (-b + (discr) ** 0.5) / (2 * a)
+        x2 = (-b - (discr) ** 0.5) / (2 * a)
+        print("x1 =", x1, "x2 =", x2)
+    elif discr == 0:
+        x = -b / (2 * a)
+        print("x =", x)
+    else:  # discr < 0
+        print("Корней нет.")
+    input("Нажмите Enter для завершения...")
 
 
-
-
-
-
-
-
- 
+if __name__ == "__main__":
+    test()
